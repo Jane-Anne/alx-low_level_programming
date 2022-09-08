db29d7ee736bbf2234adc4117c670cedce8b0937
Jane-Anne <janeeastwood06@outlook.com>
2022-09-08 11:26:43 -0700

    Task 4 Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+{
+	//string initialisation
+	char Mystr[] =  "Programming is like building a multilingual puzzle";
+	
+	puts(Mystr); //writing the string to output-stdout
+
+	    
+
+	    return 0;
+
+}
