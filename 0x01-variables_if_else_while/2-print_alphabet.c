d96ae5b7903ff7f57fa382b11d26cc30ea600443
Jane-Anne <janeeastwood06@outlook.com>
2022-09-09 16:12:38 -0700

    Task 2 Print alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * program that putchar to prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char love;
+
+	for (love = 'a'; love <= 'z'; love++)
+		putchar(love);
+	putchar('\n');
+
+	return (0);
+}
