d8cd23e64ff1be43577f4eabd9240ffcad67766d
Jane-Anne <janeeastwood06@outlook.com>
2022-09-10 07:54:32 -0700

    Task 0 Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point prints what random number is
  *
- * Description: finds whether random value is negative. zero or positive
+ * If statements else to finds whether random value is negative. zero or positive
  *
  * return: Always zero 0 (Success)
  */
