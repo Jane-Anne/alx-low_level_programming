16d91da48cb4aa471aeb7f942eab4b21451722f9
Jane-Anne <janeeastwood06@outlook.com>
2022-09-13 12:24:29 -0700

    Task 0 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (Successful)
+ */
+
+int main(void)
+{
+	char ch = 'p';
+//write the character to stdout
+	putchar(ch);
+
+	return (0);
+}
