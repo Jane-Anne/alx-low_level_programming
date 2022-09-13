70ba507ac32788730495c87c67ea96025c17db53
Jane-Anne <janeeastwood06@outlook.com>
2022-09-13 16:34:34 -0700

    Task 2 print alphabet10x in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main 
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return - Always zero 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	int c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
+
 		{
 			_putchar(c);
 		}
