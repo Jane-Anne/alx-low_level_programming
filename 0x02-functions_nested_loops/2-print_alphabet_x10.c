e754c49fb59af5e160e387e6ed6af05de64599f7
Jane-Anne <janeeastwood06@outlook.com>
2022-09-13 16:21:00 -0700

    Task 2 print alphabet10x in lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+=include "main"
+
+/**
+ * main 
+ *
+ * Return - Always zero 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int c, i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c<= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+}
