00b52b2c83901db61f4889742e280c018f08369d
Jane-Anne <janeeastwood06@outlook.com>
2022-09-13 15:42:58 -0700

    Task 1 print alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ *  main - print alphabets in lower case
+ *
+ *   Return: Always 0.
+ *
+ **/
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+_putchar('\n');
+}
