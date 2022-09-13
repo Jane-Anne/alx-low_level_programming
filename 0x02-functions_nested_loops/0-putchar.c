7390500bd208a518b54c2f6e4610bfd188dab392
Jane-Anne <janeeastwood06@outlook.com>
2022-09-13 14:46:50 -0700

    Task 0 _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <main.h>
 /**
  * main - Entry point
  *
@@ -9,9 +9,15 @@
 
 int main(void)
 {
-	char ch = 'p';
-//write the character to stdout
-	putchar(ch);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n')
 
 	return (0);
 }
