acd5528fe9d34ca7f69933e20c669a7f11046c46
Jane-Anne <janeeastwood06@outlook.com>
2022-09-14 01:05:49 -0700

    Task 10 natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_to_98 - prints all natural numbers from 98
+ *separate with comma then space
+ * 
+ * @n: beging counting
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ",n--);
+		print("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
