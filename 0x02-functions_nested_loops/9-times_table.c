3c3fdbe6b204204182a27d293bb4680c69b2a93d
Jane-Anne <janeeastwood06@outlook.com>
2022-09-14 00:18:45 -0700

    Task 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0: num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 		
@@ -18,7 +18,7 @@ void times_table(void)
 
 			prod = num * mult;
 			if (prod <= 0)
-				putchar(' ');
+				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 
