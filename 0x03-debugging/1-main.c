7b2f1d17a67584d4e9c97943018e66b850591622
Jane-Anne <janeeastwood06@outlook.com>
2022-09-15 01:05:19 -0700

    Task 1 Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-i = 0;
+	i = 0;
 
 /**
 *while (i < 10)
@@ -18,7 +18,7 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
