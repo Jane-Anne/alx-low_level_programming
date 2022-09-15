f492c671e3a925ecea7721effef4b53b85bc57d4
Jane-Anne <janeeastwood06@outlook.com>
2022-09-15 00:16:36 -0700

    Task 0 Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - will test if function positive_or_negative()
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
