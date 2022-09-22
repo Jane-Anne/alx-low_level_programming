1e1462a95c04b65b79e895c0a81568ffe32c7e75
Jane-Anne <janeeastwood06@outlook.com>
2022-09-22 13:57:16 -0700

    Task 1 strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int s = 0, i = 0;
+
+	while (dest[s] != 0)
+	{
+		s++;
+	}
+	while (src[i] != 0)
+	{
+		dest[s + i] = src[i];
+		i++;
+	}
+
+	dest[s + i] = 0;
+	return (desr);
+}
