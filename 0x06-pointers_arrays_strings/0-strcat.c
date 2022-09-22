b1e86580a853c82d3bcf1d77544740ba54d01480
Jane-Anne <janeeastwood06@outlook.com>
2022-09-22 14:24:19 -0700

    Task 0 concastates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates the string pointed 
- * @src: source
+ * *_strcat - Concatenates the two strings
  * @dest: destination
+ * @src: source
  *
- * Return: source string to be appended to @dest
+ * Return: A pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int s = 0, i = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[s] != 0)
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	{
+		s++;
+	}
 
+	while (src[i] != 0)
+	{
+		dest[s + i] = src[i];
+		i++;
+	}
+
+	dest[s + i] = 0;
 	return (dest);
 }
